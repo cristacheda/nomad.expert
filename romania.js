d2dba8cee5f3533277c02a84887e0a4b14a5b3b4
Dragoș Cristache <dragos@cristache.net>
2020-09-04 20:18:34 +0300

    Update romania.js

--- romania.js
+++ romania.js
@@ -3,7 +3,7 @@
     "Cehia" : "ID, Passport",
     "Ungaria" : "ID, Passport",
     "Franta" : "ID, Passport",
-    "tara naspa" : ["ID, Passport", "https://asasd.com", "Verifică cu autoritățile."]
+    "tara naspa" : ["ID, Passport", "https://asasd.com", "Verifică cu autoritățile."],
 }
 
 // "country name" : ["papers", "source", "comment"]
