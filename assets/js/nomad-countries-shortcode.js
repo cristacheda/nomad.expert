08f7b0b5eac98ccb16020b6f532e7135a6f890a8
Dragoș Cristache <dragos@cristache.net>
2025-01-02 15:57:22 +0200

    1.0.3

--- assets/js/nomad-countries-shortcode.js
+++ assets/js/nomad-countries-shortcode.js
@@ -9,7 +9,7 @@ countries.registerLocale(require("i18n-iso-countries/langs/ro.json"));
 		result,
 		fileName,
 		body = $('body'),
-		githubDbFolder = 'https://github.com/cristacheda/nomad.expert/tree/gh-pages/data/documents/',
+		githubDbFolder = 'https://github.com/cristacheda/nomad.expert/tree/master/',
 		availableCountries = [{
 			"code": "RO",
 			"name": "Romania",
@@ -28,24 +28,28 @@ countries.registerLocale(require("i18n-iso-countries/langs/ro.json"));
 
 	buildOriginCountrySelector(availableCountries);
 
-	// originCountrySelector.find('option')[1].attr('selected','selected');
-
 	originCountrySelector.change(function () {
 		fileName = $(this).val();
 		if (fileName) {
-			$.getJSON( "/wp-content/plugins/nomad.expert/data/documents/" + fileName )
+			//https://raw.githubusercontent.com/cristacheda/nomad.expert/refs/heads/gh-pages/data/documents/
+			//https://raw.githubusercontent.com/cristacheda/nomad.expert/refs/heads/master/
+			$.getJSON( "https://raw.githubusercontent.com/cristacheda/nomad.expert/refs/heads/master/" + fileName )
 			.done(function( json ) {
 				result = json;
 				buildDestinationCountrySelector(result);
 			})
 			.fail(function( jqxhr, textStatus, error ) {
 				var err = textStatus + ", " + error;
+				$('#result').append("Eroare în JSON: " +err).show();
 				console.log( "Request Failed: " + err );
 			});
 		} else {
 			resetDestinationCountrySelector();
 		}
 	});
+
+	originCountrySelector.find('option').eq(1).attr('selected','selected');
+	originCountrySelector.trigger('change');
 
 	function resetDestinationCountrySelector() {
 		if (destinationCountrySelector.length > 0) {
@@ -66,6 +70,42 @@ countries.registerLocale(require("i18n-iso-countries/langs/ro.json"));
 			}
 		}
 	}
+
+	// dev
+	// $(document).ready(function() {
+	// 	destinationCountrySelector.val('YE').trigger('change');
+	// });
+
+	originCountrySelector.select2({
+        placeholder: 'Selectează o țară',
+        width: '100%',
+    });
+
+	destinationCountrySelector.select2({
+        placeholder: 'Selectează o țară',
+        width: '100%',
+        
+        // Template for displaying options in the dropdown
+        templateResult: function(state) {
+            if (!state.id) {
+                return state.text; // Return the default text for placeholder or blank option
+            }
+            // Format the option with the flag and the country name
+            var flagUrl = `/wp-content/plugins/nomad.expert/assets/img/country-flags/${state.id}.SVG`;
+            var flagImage = `<span><img src="${flagUrl}" class="flag" style="width: 20px; height: 15px; margin-right: 10px;">${state.text}</span>`;
+            return $(flagImage); // Return the formatted option
+        },
+        
+        // Template for displaying the selected option
+        templateSelection: function(state) {
+            if (!state.id) {
+                return state.text; // Return the default text
+            }
+            var flagUrl = `/wp-content/plugins/nomad.expert/assets/img/country-flags/${state.id}.SVG`;
+            var flagImage = `<span><img src="${flagUrl}" class="flag" style="width: 20px; height: 15px; margin-right: 10px;">${state.text}</span>`;
+            return $(flagImage); // Return the formatted selected option
+        }
+    });
 
 	function findCountry(countryCode, result) {
 		var country = result.find(item => item.code === countryCode);
@@ -93,43 +133,61 @@ countries.registerLocale(require("i18n-iso-countries/langs/ro.json"));
 		}
 
 		if (country.emergency) {
-			result.append(`<p id="emergency" class="blockquote"><strong>⚠️ Atenție!</strong> ${country.emergency}</p>`);
+			result.append(`<div id="emergency" class="d-flex flex-lg-row flex-column mb-4">
+				<p class="col-lg-3 block-header d-flex flex-column justify-content-center align-content-center text-center p-lg-2">Alertă de călătorie!</p>
+				<p class="col-lg-9 content p-lg-2 mb-0"><strong>⚠️ Atenție!</strong> ${country.emergency}</p>
+			</div>`);
 		}
 
 		if (country.warning) {
-			result.append(`<p id="warning" class="blockquote">${country.warning}</p>`);
+			result.append(`<div id="warning" class="d-flex flex-lg-row flex-column mb-4">
+				<p class="col-lg-3 block-header d-flex flex-column justify-content-center align-content-center p-lg-2 text-center">Alertă de călătorie!</p>
+				<p class="col-lg-9 content p-lg-2 mb-0"><strong>⚠️ Atenție!</strong> ${country.warning}</p>
+			</div>`);
 		}
 
 		if (country.comment) {
-			result.append(`<p id="comment" class="blockquote">${country.comment}</p>`);
+			result.append(`<div id="comment" class="${documentclass} d-flex flex-lg-row flex-column mb-4">
+				<p class="col-lg-3 block-header d-flex flex-column justify-content-center align-content-center p-lg-2 text-center">Condiții de intrare</p>
+				<p class="col-lg-9 content p-lg-2 mb-0">${country.comment}</p>
+			</div>`);
+		}
+
+		if (country.misiune || country.vaccine || country.affiliation) {
+			result.append(`<div id="extra" class="d-flex flex-lg-row flex-column">
+				<p class="col-lg-3 block-header d-flex flex-column justify-content-center align-content-center p-lg-2 text-center"></p>
+				<div class="col-lg-9 content p-lg-2"></div>
+			</div>`);
 		}
 
 		if (country.misiune) {
-			result.append(`<p id="mission" class="blockquote">${country.misiune}</p>`);
+			result.find("#extra .block-header").append(`Misiune diplomatică<br><br>`);
+			result.find("#extra .content").append(`<p id="mission">${country.misiune}</p>`);
 		}
 
 		if (country.vaccine) {
-			result.append(`<p id="vaccine">${country.vaccine}</p>`);
+			result.find("#extra .block-header").append(`Vaccin<br><br>`);
+			result.find("#extra .content").append(`<p id="vaccine">${country.vaccine}</p>`);
 		}
 
 		if (country.affiliation) {
-			result.append(`<p id="affiliation">Afiliere: ${country.affiliation}</p>`);
+			result.find("#extra .block-header").append(`Afiliere<br><br>`);
+			result.find("#extra .content").append(`<p id="affiliation">Afiliere: ${country.affiliation}</p>`);
 		}
 
 		if (country.source) {
 			result.append(`<p id="source"><a target="_blank" rel="noopener" href="${country.source}">Sursa informațiilor</a></p>`);
 
-			$.getJSON('https://restcountries.eu/rest/v2/alpha/' + country.code + '?fields=capital;population;timezones;currencies;languages', function (json) {
-				$('#source').before('<ul id="facts"></ul>');
+			$.getJSON('https://restcountries.com/v3.1/alpha/' + country.code + '?fields=capital,population,timezones,currencies,languages', function (json) {
+				result.find("#extra .content").append('<ul id="facts" class="pl-3"></ul>');
 				var facts = $('#facts');
 				if (json) {
 					// console.log(json);
-					facts.append(`<li class="first"><strong>Fun facts!</strong></li>`);
 					facts.append(`<li>Capitala: ${json.capital}</li>`);
 					facts.append(`<li>Populație: ${json.population.toLocaleString()}</li>`);
 					facts.append(`<li>Fus orar principal: ${json.timezones[0]}</li>`);
-					facts.append(`<li>Moneda principală: ${json.currencies[0].name}</li>`);
-					facts.append(`<li>Limba principală: ${json.languages[0].name}</li>`);
+					facts.append(`<li>Moneda principală: ${json.currencies[Object.keys(json.currencies)[0]].name}</li>`);
+					facts.append(`<li>Limba principală: ${json.languages[Object.keys(json.languages)[0]]}</li>`);
 					if (country.funfact) {
 						facts.append(`<li>${country.funfact}</li>`);
 					}
@@ -138,7 +196,7 @@ countries.registerLocale(require("i18n-iso-countries/langs/ro.json"));
 		}
 
 		if (fileName && githubDbFolder) {
-			result.append(`<p id="edit"><a target="_blank" rel="noopener" href="${githubDbFolder}/${fileName}"><i class="fal fa-edit"></i> Corectează informațiile</a></p>`);
+			result.append(`<p id="edit"><a target="_blank" rel="noopener" href="${githubDbFolder}${fileName}"><i class="fal fa-edit"></i> Corectează informațiile</a></p>`);
 		}
 
 		result.show();
--- nomad-countries-shortcode.php
+++ nomad-countries-shortcode.php
@@ -3,14 +3,17 @@
 /**
  *
  * @link              https://cristache.net
- * @since             1.0.1
+ * @since             1.0.3
  * @package           Nomad_Countries_Shortcode
  *
  * @wordpress-plugin
  * Plugin Name:       Nomad Countries Shortcode
  * Plugin URI:        https://nomad.expert
  * Description:       
- * Version:           1.0.1
+ * Tested up to:      6.7.1
+ * Version:           1.0.3
+ * Requires at least: 6.7.1
+ * Requires PHP:      7.4
  * Author:            Dragos Cristache
  * Author URI:        https://cristache.net
  */
@@ -22,10 +25,20 @@ if ( ! defined( 'WPINC' ) ) {
 
 function utm_user_scripts() {
     $plugin_url = plugin_dir_url( __FILE__ );
-    wp_enqueue_style( 'style',  $plugin_url . "assets/css/nomad-countries-shortcode.css" );
-    wp_enqueue_script( 'script',  $plugin_url . "assets/js/nomad-countries-shortcode.min.js", array('jquery'), null, true  );
+    wp_enqueue_style( 'style',  $plugin_url . "assets/css/nomad-countries-shortcode.css", array(), filemtime(plugin_dir_path(__FILE__) . "assets/css/nomad-countries-shortcode.css"), 'all' );
+    wp_enqueue_style( 'fontawesome',  "https://fa.leadgap.ro/latest/css/all.min.css" );
+    wp_enqueue_style( 'select2',  "https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" );
+    wp_enqueue_script( 'select2',  "https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js", array('jquery'), null, true  );
+    wp_enqueue_script( 'script',  $plugin_url . "assets/js/nomad-countries-shortcode.min.js", array('jquery'), filemtime(plugin_dir_path(__FILE__) . "assets/js/nomad-countries-shortcode.min.js"), true  );
 }
 add_action( 'wp_enqueue_scripts', 'utm_user_scripts' );
+
+// Elementor - Remove Font Awesome 
+add_action( 'elementor/frontend/after_register_styles',function() {
+	foreach( [ 'solid', 'regular', 'brands' ] as $style ) {
+		wp_deregister_style( 'elementor-icons-fa-' . $style );
+	}
+}, 20 );
 
 add_shortcode( 'nomad_render_html', 'nomad_render_html' );
 function nomad_render_html() {
@@ -41,7 +54,7 @@ function nomad_render_html() {
 				<div class="row">
 					<div class="col-md-6 col-12">
 						<label for="origin-country-selector">Din ce țară călătorești?</label>
-						<select id="origin-country-selector"></select>
+						<select disabled id="origin-country-selector"></select>
 					</div>
 					<div class="col-md-6 col-12" id="destination-country-selector-column">
 						<label for="destination-country-selector">și în ce țară vrei să ajungi?</label>
